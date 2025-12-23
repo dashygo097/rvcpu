9e796eeac9767de6ceb5b3361c248fbc61670a1c
dashygo097 <dashygo097@github.com>
2025-12-23 17:52:47 +0800

    rename

--- core/EX/Alu.scala
+++ core/EX/Alu.scala
@@ -9,22 +9,22 @@ class RV32ALU extends Module {
 
   val rs1_data = IO(Input(UInt(32.W))).suggestName("ALU_RS1_DATA")
   val rs2_data = IO(Input(UInt(32.W))).suggestName("ALU_RS2_DATA")
-  val rd_data  = IO(Output(UInt(32.W))).suggestName("ALU_RD")
+  val result   = IO(Output(UInt(32.W))).suggestName("ALU_RESULT")
 
   // control signals
-  val is_alu_sub = IO(Input(Bool())).suggestName("IS_ALU_SUB")
-  val is_alu_sra = IO(Input(Bool())).suggestName("IS_ALU_SRA")
+  val alu_is_sub = IO(Input(Bool())).suggestName("ALU_IS_SUB")
+  val alu_is_sra = IO(Input(Bool())).suggestName("ALU_IS_SRA")
   val alu_op     = IO(Input(UInt(3.W))).suggestName("ALU_OP")
 
-  rd_data := MuxLookup(alu_op, 0.U)(
+  result := MuxLookup(alu_op, 0.U)(
     Seq(
-      ALUOp.ADD  -> Mux(is_alu_sub, rs1_data - rs2_data, rs1_data + rs2_data),
+      ALUOp.ADD  -> Mux(alu_is_sub, rs1_data - rs2_data, rs1_data + rs2_data),
       ALUOp.SLL  -> (rs1_data << rs2_data(4, 0)),
       ALUOp.SLT  -> Mux(rs1_data.asSInt < rs2_data.asSInt, 1.U, 0.U),
       ALUOp.SLTU -> Mux(rs1_data < rs2_data, 1.U, 0.U),
       ALUOp.XOR  -> (rs1_data ^ rs2_data),
       ALUOp.SRL  -> Mux(
-        is_alu_sra,
+        alu_is_sra,
         (rs1_data.asSInt >> rs2_data(4, 0)).asUInt,
         rs1_data >> rs2_data(4, 0)
       ),
