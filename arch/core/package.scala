fe4642caf62cad45c38fa22a82c4dd283535cbe6
dashygo097 <dashygo097@github.com>
2025-12-26 19:34:00 +0800

    [arch]: feat: pipeline stage

--- arch/core/package.scala
+++ arch/core/package.scala
@@ -1,5 +1,9 @@
 package arch.core
 
+package object pipeline {}
+
+package object common {}
+
 package object decoder {}
 
 package object alu {}
---
+++ arch/core/pipeline.scala
@@ -0,0 +1,21 @@
+package arch.core.pipeline
+
+import arch.configs._
+import chisel3._
+
+class PipelineStage[T <: Data](gen: T)(implicit p: Parameters) extends Module {
+  override def desiredName: String = s"${p(ISA)}_pipeline_stage_${gen.getClass.getSimpleName}"
+
+  val stall = IO(Input(Bool()))
+  val flush = IO(Input(Bool()))
+  val sin   = IO(Input(gen.cloneType))
+  val sout  = IO(Output(gen.cloneType))
+
+  when(flush) {
+    sout := 0.U.asTypeOf(gen)
+  }.elsewhen(!stall) {
+    sout := sin
+  }.otherwise {
+    sout := sout
+  }
+}
